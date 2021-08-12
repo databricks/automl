8fd39c3ca842e9934fcea3b1db0bafa64e2dcf42
lu-wang-dl <lu.wang@databricks.com>
2021-08-12 12:12:22 -0700

    fix

--- dev-requirements.py
+++
@@ -1,8 +0,0 @@
-# Packages required for the development and testing environment
-# Instructions:
-# * Re-build and update `:docker-env` after change.
-#   See https://databricks.atlassian.net/wiki/spaces/UN/pages/584286711/Bazel+Docker+Rules#BazelDockerRules-Rebuildingdocker_baselayers.
-# * Keep dependencies sorted.
-pytest~=6.2.1
-pytest-cov~=2.11.1
-testfixtures~=6.17.1
