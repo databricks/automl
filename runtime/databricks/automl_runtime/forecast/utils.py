b0b0ce60d6c3e8f61eab6e7a05b2597b7deb8760
Shitao Li <shitao.li@databricks.com>
2022-10-28 10:44:53 -0700

    fix.

--- runtime/databricks/automl_runtime/forecast/utils.py
+++ runtime/databricks/automl_runtime/forecast/utils.py
@@ -67,7 +67,11 @@ def generate_cutoffs(df: pd.DataFrame, horizon: int, unit: str,
                           the same frequency unit as the time series.
     :return: list of pd.Timestamp cutoffs for cross-validation.
     """
-    period = max(horizon // 2, 1)  # avoid empty cutoff buckets
+    period = max(0.5 * horizon, 1)  # avoid empty cutoff buckets
+
+    # avoid non-integer months, quaters ands years
+    if unit == 'MS' or unit == 'QS' or unit == 'YS':
+        period = int(period)
 
     period_dateoffset = pd.DateOffset(**dict({DATE_OFFSET_KEYWORD_MAP[unit] : period}))
     horizon_dateoffset = pd.DateOffset(**dict({DATE_OFFSET_KEYWORD_MAP[unit] : horizon}))
