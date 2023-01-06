d7c5153cab9be3cfcd78f95a547699d437998fd2
lu-wang-dl <lu.wang@databricks.com>
2023-01-05 17:59:32 -0800

    add interpolation transformer

---
+++ runtime/databricks/automl_runtime/sklearn/interpolation_imputer.py
@@ -0,0 +1,69 @@
+#
+# Copyright (C) 2023 Databricks, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from __future__ import annotations
+from typing import Any, Dict
+
+import pandas as pd
+from sklearn.base import TransformerMixin, BaseEstimator
+
+DEFAULT_PARAMS = dict(
+        method="linear",
+        limit_direction="both",
+        axis=0)
+
+
+class InterpolationImputer(TransformerMixin, BaseEstimator):
+    """Null value Imputer by interpolation
+    """
+
+    def __init__(self,
+                 *,
+                 impute_params: Dict[str, Any]=None,
+                 impute_all: boolean=True,
+        ):
+        """Create a `DatetimeImputer`.
+
+        Parameters
+        ----------
+        impute_params: `dict` or None, default None
+        Params to pass to the imputation algorithm.
+        See `pandas.DataFrame.interpolate` for their respective options.
+
+        impute_all: boolean, default True
+        Indicator to decide whether to impute all null values. If True, if the custom
+        imutation method cannot fill in all null values, use the default method if fill
+        in the nulls.
+        """
+        self._impute_params = impute_params or DEFAULT_PARAMS
+        self._impute_all = impute_all
+
+    def fit(self, X: pd.DataFrame, y: pd.DataFrame=None) -> "InterpolationImputer":
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """Impute the missing values with `pandas.DataFrame.interpolate`.
+
+        Parameters
+        ----------
+        X: a pandas DataFrame whose values are date or timestamp.
+        """
+        assert isinstance(X, pd.DataFrame)
+
+        X_imputed = X.interpolate(**self._impute_params)
+        if self._impute_all:
+            X_imputed = X_imputed.interpolate(**DEFAULT_PARAMS)
+
+        return X_imputed
---
+++ runtime/tests/automl_runtime/sklearn/interpolation_imputer_test.py
@@ -0,0 +1,48 @@
+#
+# Copyright (C) 2022 Databricks, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import numpy as np
+import pandas as pd
+from pandas.testing import assert_frame_equal
+import unittest
+
+from databricks.automl_runtime.sklearn import InterpolationImputer
+
+class TestInterpolationImputer(unittest.TestCase):
+    def setUp(self) -> None:
+        self.df = pd.DataFrame([(0.0, np.nan, -1.0, 1.0),
+                                (np.nan, 2.0, np.nan, np.nan),
+                                (2.0, 3.0, np.nan, 9.0),
+                                (np.nan, 4.0, -4.0, 16.0)],
+                               columns=list('abcd'))
+        self.expected_df = pd.DataFrame([(0.0, 2.0, -1.0, 1.0),
+                                         (1.0, 2.0, -2.0, 5.0),
+                                         (2.0, 3.0, -3.0, 9.0),
+                                         (2.0, 4.0, -4.0, 16.0)],
+                                        columns=list('abcd'))
+    def test_imputer(self):
+        assert_frame_equal(
+            InterpolationImputer().transform(self.df), self.expected_df)
+
+    def test_imputer_with_custom_method(self):
+        expected_df = pd.DataFrame([(0.0, 2.0, -1.0, 1.0),
+                                    (0.0, 2.0, -1.0, 1.0),
+                                    (2.0, 3.0, -1.0, 9.0),
+                                    (2.0, 4.0, -4.0, 16.0)],
+                                   columns=list('abcd'))
+        interpolate_param = {"method": 'pad', "limit": 2}
+        imputed_df = InterpolationImputer(impute_params=interpolate_param).transform(self.df)
+        assert_frame_equal(imputed_df, expected_df)
